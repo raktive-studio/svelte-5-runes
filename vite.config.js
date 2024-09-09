b40f19b9b5f6dc03988876dee0c15995114ae15b
Özgür Can Altınok <ozgurcanaltinok@gmail.com>
2024-09-09 07:10:41 +0000

    update vite settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+//@ts-ignore
+import { sveltekit } from "@sveltejs/kit/vite";
 
+//@ts-ignore
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    https: false,
+  },
 };
 
 export default config;
